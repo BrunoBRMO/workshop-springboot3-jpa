dd3804dd77a09378b4f10867933b8ab38d674f59
Bruno Raphael <bruno.rmmo00@gmail.com>
2023-11-24 21:45:07 -0300

    Projeto criado

---
+++ src/main/java/com/projetobruno/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.projetobruno.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //anotation para que seja uma aplicação Spring boot
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
